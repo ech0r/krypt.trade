15dfe4028f1e679a8120929fcaba02f20413642c
ech0r <jrcoop34@gmail.com>
2020-01-04 00:56:36 -0800

    Updating class with another function

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ from secrets import * # Secrets file containing api key, etc.
 
 class RoboTrader:
     
-    def __init__(self, api_key):
+    def __init__(self, api_key, symbol):
         self.headers = {'X-MBX-APIKEY': api_key}
         self.exchange = "https://api.binance.com/"
+        self.params = {'symbol': symbol}
     
-    def get_historical_data(self, symbol):
+    def get_historical_data(self):
         url = self.exchange + "api/v3/historicalTrades"
-        params = {'symbol': symbol}
-        req = requests.get(url, params=params, headers=self.headers)
+        req = requests.get(url, params=self.params, headers=self.headers)
+        return req.json()
+    
+    def get_current_avg(self):
+        url = self.exchange + "api/v3/avgPrice"
+        req = requests.get(url, params=self.params, headers=self.headers)
         return req.json()
     
 
-
 if __name__ == "__main__":
-    robot = RoboTrader(api_key)
-    print(robot.get_historical_data('XRPUSDT'))
+    xrp_robot = RoboTrader(api_key, 'XRPUSDT')
+    #print(robot.get_historical_data())
+    print(xrp_robot.get_current_avg())
+
