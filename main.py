0c25c5178ec8ba56b24b82ea964df342eb4665d4
ech0r <jrcoop34@gmail.com>
2020-01-04 00:47:41 -0800

    Creating RoboTrader class

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
 import requests
+import json
 from secrets import * # Secrets file containing api key, etc.
 
+class RoboTrader:
+    
+    def __init__(self, api_key):
+        self.headers = {'X-MBX-APIKEY': api_key}
+        self.exchange = "https://api.binance.com/"
+    
+    def get_historical_data(self, symbol):
+        url = self.exchange + "api/v3/historicalTrades"
+        params = {'symbol': symbol}
+        req = requests.get(url, params=params, headers=self.headers)
+        return req.json()
+    
 
 
 if __name__ == "__main__":
-    headers = {'X-MBX-APIKEY': api_key}
-    xrp = {'symbol': 'XRPUSDT'}
-    r = requests.get("https://api.binance.com/api/v3/historicalTrades", params=xrp, headers=headers)
-    print(r.text)
+    robot = RoboTrader(api_key)
+    print(robot.get_historical_data('XRPUSDT'))
